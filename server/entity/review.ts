dca1b8d974bfd9829b66ee5be0d826b8c8cf9e38
Alexander <alexander.everaert@student.howest.be>
2021-10-29 11:07:50 +0200

    added graphql entities

--- server/entity/review.ts
+++ server/entity/review.ts
@@ -1,28 +1,41 @@
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
-import { Room } from "./room";
-import { User } from "./user";
+import { Field, ID, InputType, ObjectType } from 'type-graphql'
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm'
+import { Room } from './room'
+import { User } from './user'
 
+@ObjectType()
+@InputType('ReviewInput')
 @Entity('review')
 export class Review {
+  @Field(() => ID, { nullable: true })
+  @PrimaryGeneratedColumn('uuid')
+  reviewId?: string
 
-    @PrimaryGeneratedColumn('uuid')
-    reviewId?: string
+//   @Field(type => User)
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'userId' })
+  user?: User
 
-    @ManyToOne(() => User)
-    @JoinColumn({ name: 'userId' })
-    user?: User
+//   @Field( type => Room)
+  @ManyToOne(() => Room)
+  @JoinColumn({ name: 'roomId' })
+  room?: Room
 
-    @ManyToOne(() => Room)
-    @JoinColumn({ name: 'roomId' })
-    room?: Room
+  @Field()
+  @Column('int')
+  reviewScore?: number
 
-    @Column('int')
-    reviewScore?: number
+  @Field()
+  @Column('text')
+  title?: string
 
-    @Column('text')
-    title?: string
-
-    @Column('text')
-    description?: string
-
+  @Field()
+  @Column('text')
+  description?: string
 }
--- server/entity/roomType.ts
+++ server/entity/roomType.ts
@@ -1,18 +1,23 @@
+import { Field, ID, InputType, ObjectType } from "type-graphql";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-
+@ObjectType()
+@InputType('RoomTypeInput')
 @Entity('roomType')
 export class RoomType {
-
+    @Field(() => ID, { nullable: true })
     @PrimaryGeneratedColumn('uuid')
     roomTypeId?: string
 
+    @Field()
     @Column('text')
     typeName?: string
 
+    @Field()
     @Column('text')
     description?: string
 
+    @Field()
     @Column()
     capacity?: number
 
