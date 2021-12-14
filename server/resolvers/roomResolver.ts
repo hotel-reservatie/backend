f516669c1d014d33ed6cd27b2be043a9be1400ff
EveraertAlexander <alexander.everaert@student.howest.be>
2021-12-14 11:32:40 +0100

    test new query

--- server/resolvers/roomResolver.ts
+++ server/resolvers/roomResolver.ts
@@ -23,6 +23,22 @@ export class RoomResolver {
   }
 
   @Query(() => [Room], { nullable: true })
+  async getAllRoomsTest(): Promise<Room[]> {
+    const rooms = await this.repository.find({
+      relations: ['roomType', 'tags'],
+      join: {
+        alias: 'room',
+        leftJoinAndSelect: {
+          "reviews": "room.reviews",
+          "user": "reviews.user"
+        }
+      }
+    })
+
+    return rooms
+  }
+
+  @Query(() => [Room], { nullable: true })
   async getRooms(@Arg('Filters', { nullable: true }) filters: RoomFilters) {
     try {
 
