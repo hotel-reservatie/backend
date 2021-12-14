a16e6e8a2e23d8268cbc25cbc04d73b304cb28c4
EveraertAlexander <alexander.everaert@student.howest.be>
2021-12-14 12:03:36 +0100

    0.0.1

--- server/app.ts
+++ server/app.ts
@@ -64,7 +64,7 @@ import { FilterResolver } from './resolvers/filterResolver'
 
       // ROUTES
       app.get('/', (request: Request, response: Response) => {
-        response.send(`Tested with new github actions pipeline!`)
+        response.send(`This has been updated automatically via github actions`)
       })
       /**
        *
