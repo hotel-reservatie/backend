1a27c67d90d7f36a9459d69e1c3849b4c11842ba
Alexander Everaert <alexander.everaert@student.howest.be>
2021-12-18 17:05:04 +0100

    fix signup route

--- server/controllers/auth.controller.ts
+++ server/controllers/auth.controller.ts
@@ -15,44 +15,56 @@ export class AuthController {
 
   signup = async (request: Request, response: Response, next: NextFunction) => {
     const { email, password, name } = request.body
-    try {
-      const user = await auth().createUser({
-        email,
-        password,
-        displayName: name,
-      })
 
-      const customClaims = {
-        admin: false,
-      }
-      //Users with howest email get admin rights
-      if (user.email && user.email.endsWith('@howest.be')) {
-        customClaims.admin = true
-      }
+
+    if (email && password && name) {
       try {
-        await getAuth().setCustomUserClaims(user.uid, customClaims)
-      } catch (error) {
-        console.log(error)
-      }
-
-      const u: User = new User()
-
-      u.userId = user.uid
-      u.userName = user.displayName
-      u.email = user.email
-      u.phone = user.phoneNumber
-      u.admin = customClaims.admin
-
-      await this.userRepository.save(u)
-        .then(() => console.log("Added new user to database: ", u))
-        .catch((e) => {
-          console.log(`Could not insert user: ${e}`);
+        const user = await auth().createUser({
+          email,
+          password,
+          displayName: name,
         })
 
-      return response.json(user)
-    } catch (error) {
-      response.status(400)
-      return response.json(error)
+        const customClaims = {
+          admin: false,
+        }
+        //Users with howest email get admin rights
+        if (user.email && user.email.endsWith('@howest.be')) {
+          customClaims.admin = true
+        }
+        try {
+          await getAuth().setCustomUserClaims(user.uid, customClaims)
+        } catch (error) {
+          console.log(error)
+        }
+
+        const u: User = new User()
+
+        u.userId = user.uid
+        u.userName = name
+        u.email = email
+        u.phone = user.phoneNumber
+        u.admin = customClaims.admin
+
+        console.log(u);
+        console.log(user);
+
+
+
+        await this.userRepository.save(u)
+          .then(() => console.log("Added new user to database: ", u))
+          .catch((e) => {
+            console.log(`Could not insert user: ${e}`);
+          })
+
+        return response.json(user)
+      } catch (error) {
+        response.status(400)
+        return response.json(error)
+      }
     }
+
+    response.status(400)
+    return response.json({"error": "Bad request"})
   }
 }
